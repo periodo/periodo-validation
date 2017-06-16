0ccbcceb1bca3d3158d3bd61e260775aa3ae9425
Ryan Shaw <ryanshaw@unc.edu>
2017-06-16 11:23:28 -0400

    Reorganize and document script

--- changes/proposed/whitelist-language-tags/fix-language-tags/index.js
+++ changes/proposed/whitelist-language-tags/fix-language-tags/index.js
@@ -9,44 +9,56 @@ const R = require('ramda')
     , request = require('request')
     , tags = require('language-tags')
 
+// Buffer => String
 const toString = buf => buf.toString('utf8')
 
+// Buffer => Object
 const parseJSON = R.pipe(toString, JSON.parse)
 
-const JSONPointer = R.pipe(
-  R.map(element => element === '$' ? '' : element),
-  R.join('/')
-)
+// Object => String
+const prettify = doc => JSON.stringify(doc, null, 2)
 
-const throwUnexpected = name => value => {
-  throw `unexpected ${name}: ${value}`
+// JSON Patch ------------------------------------------------------------------
+
+// JSONPatch => JSONPatch
+const validatePatch = patch => {
+  const error = jsonpatch.validate(patch)
+  if (error) {
+    console.error(error)
+    throw error
+  }
+  return patch
 }
 
-const extractTags = R.pipe(
-  R.cond(
-    [ [ R.is(String), Array.of ]
-    , [ R.is(Object), R.keys ]
-    , [ R.T, throwUnexpected('value type') ]
-    ]
-  )
+// Object => JSONPatch => Object
+const applyPatch = doc => patch => jsonpatch.applyPatch(doc, patch).newDocument
+
+// -----------------------------------------------------------------------------
+
+// Error => ErrorInfo
+const errorInfo = e => (
+  { message: e.toString()
+  , code: e.code
+  , tag: e.tag
+  , subtag: e.subtag
+  }
 )
 
-const extractTagPaths = R.reduce(
-  (tagpaths, {path, value}) => (
-    R.concat(tagpaths, R.map(tag => ({path, tag}), extractTags(value)))
-  ), []
-)
+// String => Array<ErrorInfo>
+const errors = tag => R.map(errorInfo, tags(tag).errors())
 
-const iso2to1 = languageTag => langs.has('2', languageTag)
-  ? langs.where('2', languageTag)['1']
-  : languageTag
-
-const fixLanguage = tag => {
-  const [iso2lang, script] = R.split('-', tag)
-      , iso1lang = iso2to1(iso2lang)
-  return script ? R.join('-', [iso1lang, script]) : iso1lang
+// String => String
+const assertValid = tag => {
+  if (! tags.check(tag)) {
+    throw JSON.stringify(errors(tag), null, 2)
+  }
+  return tag
 }
 
+// String => String
+const format = tag => tags(tag).format()
+
+// String => Boolean
 const hasSuppressedScript = tag => {
   const subtags = tags(tag).subtags()
       , language = subtags[0]
@@ -59,6 +71,7 @@ const hasSuppressedScript = tag => {
   )
 }
 
+// String => String
 const fixScript = tag => {
   const [language] = R.split('-', tag)
   return hasSuppressedScript(tag)
@@ -66,25 +79,19 @@ const fixScript = tag => {
     : tag
 }
 
-const format = tag => tags(tag).format()
+// String => String
+const iso2to1 = languageTag => langs.has('2', languageTag)
+  ? langs.where('2', languageTag)['1']
+  : languageTag
 
-const errorInfo = e => (
-  { message: e.toString()
-  , code: e.code
-  , tag: e.tag
-  , subtag: e.subtag
-  }
-)
-
-const errors = tag => R.map(errorInfo, tags(tag).errors())
-
-const assertValid = tag => {
-  if (! tags.check(tag)) {
-    throw JSON.stringify(errors(tag), null, 2)
-  }
-  return tag
+// String => String
+const fixLanguage = tag => {
+  const [iso2lang, script] = R.split('-', tag)
+      , iso1lang = iso2to1(iso2lang)
+  return script ? R.join('-', [iso1lang, script]) : iso1lang
 }
 
+// String => String
 const replacements = R.cond([
   // https://github.com/periodo/periodo-data/issues/38
   [ R.equals('ell-latn'), R.always('ell-grek') ],
@@ -93,6 +100,7 @@ const replacements = R.cond([
   [ R.T, R.identity ],
 ])
 
+// String => String
 const fix = R.pipe(
   replacements,
   fixLanguage,
@@ -101,13 +109,13 @@ const fix = R.pipe(
   assertValid,
 )
 
-const replaceOperation = ({tag, path}) => (
-  { op: 'replace'
-  , value: fix(tag)
-  , path: JSONPointer(path)
-  }
+// Array<JSONPathElement> => JSONPointer
+const JSONPointer = R.pipe(
+  R.map(element => element === '$' ? '' : element),
+  R.join('/')
 )
 
+// {path: Array<JSONPathElement>, tag: String} => MoveOperation
 const moveOperation = ({tag, path}) => (
   { op: 'move'
   , from: JSONPointer(R.append(tag, path))
@@ -115,40 +123,63 @@ const moveOperation = ({tag, path}) => (
   }
 )
 
+// {path: Array<JSONPathElement>, tag: String} => ReplaceOperation
+const replaceOperation = ({tag, path}) => (
+  { op: 'replace'
+  , value: fix(tag)
+  , path: JSONPointer(path)
+  }
+)
+
+// {path: Array<JSONPathElement>} => Boolean
 const lastPathElementIs = element => R.pipe(
   R.prop('path'),
   R.last,
   R.equals(element)
 )
 
+// String => String => throws
+const throwUnexpected = name => value => {
+  throw `unexpected ${name}: ${value}`
+}
+
+// {path: Array<JSONPathElement>, tag: String} => Operation or throws
 const operation = R.cond([
   [ lastPathElementIs('language'), replaceOperation ],
   [ lastPathElementIs('localizedLabels'), moveOperation ],
   [ R.T, throwUnexpected('element') ]
 ])
 
-const createPatch = R.map(operation)
+// Anything => Array<String> or throws
+const extractTags = R.pipe(
+  R.cond(
+    [ [ R.is(String), Array.of ]
+    , [ R.is(Object), R.keys ]
+    , [ R.T, throwUnexpected('value type') ]
+    ]
+  )
+)
 
+// String => Object => Array<JsonPathNode>
 const find = query => doc => jsonpath.nodes(doc, query)
 
-const validatePatch = patch => {
-  const error = jsonpatch.validate(patch)
-  if (error) {
-    console.error(error)
-    throw error
-  }
-  return patch
-}
+// Array<JsonPathNode> => Array<{path: Array<JSONPathElement>, tag: String}>
+const extractTagPaths = R.reduce(
+  (tagpaths, {path, value}) => (
+    R.concat(tagpaths, R.map(tag => ({path, tag}), extractTags(value)))
+  ), []
+)
 
-const applyPatch = doc => patch => jsonpatch.applyPatch(doc, patch).newDocument
-
-const prettify = doc => JSON.stringify(doc, null, 2)
+// Object => Array<Operation>
+const createPatch = R.pipe(
+  find('$.periodCollections[*].definitions[*]["language","localizedLabels"]'),
+  extractTagPaths,
+  R.map(operation)
+)
 
 const flags = parseArgs(process.argv.slice(2), {boolean: ['a', 'apply']})
 
-const processDoc = doc => R.pipe(
-  find('$.periodCollections[*].definitions[*]["language","localizedLabels"]'),
-  extractTagPaths,
+const processDocument = doc => R.pipe(
   createPatch,
   validatePatch,
   R.ifElse(
@@ -160,7 +191,7 @@ const processDoc = doc => R.pipe(
   console.log,
 )(doc)
 
-const processBuffer = R.pipe(parseJSON, processDoc)
+const processBuffer = R.pipe(parseJSON, processDocument)
 
 request('http://n2t.net/ark:/99152/p0d.json')
   .pipe(concat(processBuffer))
