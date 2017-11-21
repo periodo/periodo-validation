2bff81bb167c0ac73f728defe5718a9717a14d13
Ryan Shaw <ryanshaw@unc.edu>
2017-11-21 14:24:41 -0500

    Allow overriding default dataset URL

--- fix/index.js
+++ fix/index.js
@@ -11,9 +11,9 @@ const R = require('ramda')
 const toString = buf => buf.toString('utf8')
 
 // () => Promise
-const fetchCanonicalData = new Promise(
+const fetchData = url => new Promise(
   (resolve, reject) => {
-    request('http://n2t.net/ark:/99152/p0d.json')
+    request(url)
       .on('error', reject)
       .pipe(concat(R.pipe(toString, JSON.parse, resolve)))
   }
@@ -35,14 +35,23 @@ const applyPatch = doc => patch => jsonpatch.applyPatch(doc, patch).newDocument
 // Object => String
 const prettify = doc => JSON.stringify(doc, null, 2)
 
-const argv = parseArgs(process.argv.slice(2), {boolean: ['a', 'apply']})
+const argv = parseArgs(
+  process.argv.slice(2),
+  {boolean: 'apply',
+   string: 'dataset',
+   default: {dataset: 'http://n2t.net/ark:/99152/p0d.json'},
+   alias: {a: 'apply', d: 'dataset'}
+  }
+)
 
 if (argv._.length === 0) {
   console.error(
-    `Usage: node fix <proposed change> [ --apply ]
+    `Usage: node fix <proposed change> [ --apply ] [ --dataset <url> ]
 
 Outputs a patch implementing the proposed change.
---apply or -a applies the patch to the current canonical data.
+
+--apply   or -a applies the patch to the current canonical data.
+--dataset or -d specifies the URL of a dataset other than the canonical one.
 
 <proposed change> should be one of:`
   )
@@ -52,13 +61,13 @@ Outputs a patch implementing the proposed change.
 
 const createPatch = require(`./${argv._[0]}`)
 
-fetchCanonicalData
+fetchData(argv.dataset)
   .then(doc => {
     R.pipe(
       createPatch,
       validatePatch,
       R.ifElse(
-        () => argv.a || argv.apply,
+        () => argv.apply,
         applyPatch(doc),
         R.identity
       ),
