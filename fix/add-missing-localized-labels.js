8d01f2dda1c10a5003f2abcd71d3d54ab9d9f98f
Ryan Shaw <ryanshaw@unc.edu>
2020-01-22 17:16:35 -0500

    add missing localized labels

---
+++ fix/add-missing-localized-labels.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const R = require('ramda')
+    , { find, operation } = require('./utils')
+
+const fixPeriod = ({path, value}) => {
+  if (R.has('localizedLabels', value)) {
+    return []
+  }
+  if (! R.propEq('languageTag', 'en', value)) {
+    return []
+  }
+  return [
+    operation('add')(R.append('localizedLabels', path))('value')({
+      en: [ R.prop('label', value) ]
+    })
+  ]
+}
+
+// Object => Promise { Array<Operation> }
+module.exports = R.pipe(
+  find('$.authorities[*].periods[*]'),
+  R.chain(fixPeriod),
+  patch => Promise.resolve(patch)
+)
